fc4235a6b1f7c0764974c31b2d5f2db319697306
Maëva <maeva.touchet@outlook.fr>
2021-01-21 10:48:19 +0100

    Coloration d'un triangle ok

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@ using namespace std;
 const TGAColor white = { 255, 255, 255, 255 };
 const TGAColor red = { 255, 0, 0, 255};
 
+int color[width][height] = {0};
+
 struct Point{
 	int num;
 	int x;
@@ -36,8 +38,14 @@ void ligne(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor couleur){
 	int error2 = 0;
 	int y = y0;
 	for(int x=x0; x<=x1; x++){
-		if(steep)	image.set(y,x,couleur);
-		else		image.set(x,y,couleur);
+		if(steep){
+			image.set(y,x,couleur);
+			color[y][x] = 1;
+		}
+		else{
+			image.set(x,y,couleur);
+			color[x][y] = 1;
+		}
 		error2 += derror2;
 		if(error2 > dx){
 			y += (y1>y0?1:-1);
@@ -47,11 +55,62 @@ void ligne(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor couleur){
 
 }
 
+void tri(int tab[][3]){
+	int min;
+        int min2;
+        for(int i=0; i<3-1;i++){
+            int index = i;
+            for(int j= i+1; j<3; j++){
+                if(tab[1][j] < tab[1][index])     index = j;
+            }
+            min = tab[1][index];
+            min2 = tab[0][index];
+            tab[1][index] = tab[1][i];
+            tab[0][index] = tab[0][i];
+            tab[1][i] = min;
+            tab[0][i] = min2;
+	}
+}
+
+void fill_triangle(int x, int y, TGAImage &image, TGAColor couleur){
+	if((x > width) || (x < 0) || (y > height) || (y < 0))	return;
+	if(color[x][y] == 1)	return;
+	
+	image.set(x,y,couleur);
+	color[x][y] = 1;
+	cout << "On colore " << x << "," << y << endl;
+	
+	fill_triangle(x-1,y,image,couleur);
+	fill_triangle(x+1,y,image,couleur);
+	fill_triangle(x,y-1,image,couleur);
+	fill_triangle(x,y+1,image,couleur);
+}
+
+void remplir(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, TGAColor couleur){
+	int coord[2][3];
+	coord[0][0] = x0;
+	coord[1][0] = y0;
+	coord[0][1] = x1;
+	coord[1][1] = y1;
+	coord[0][2] = x2;
+	coord[1][2] = y2;
+	
+	tri(coord);
+	
+	int x = (coord[0][0] + coord[0][1] + coord[0][2])/3;
+	int y = (coord[1][0] + coord[1][1] + coord[1][2])/3; 
+	
+	image.set(x,y,couleur);
+	fill_triangle(x,y,image,couleur);
+	
+}
+
 
 void triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, TGAColor couleur){
 	ligne(x0,y0,x1,y1,image,couleur);
 	ligne(x1,y1,x2,y2,image,couleur);
 	ligne(x0,y0,x2,y2,image,couleur);
+	remplir(x0,y0,x1,y1,x2,y2,image,couleur);
 }
 
 void lecture(string name,TGAImage &image){
@@ -87,6 +146,7 @@ void lecture(string name,TGAImage &image){
 				      		point.y = y;
 				      		points.push_back(point);
 				      		image.set(x,y,white);
+				      		color[x][y] = 1;
 				      		taille++;
 			       	}
 		       	}
@@ -127,7 +187,8 @@ void lecture(string name,TGAImage &image){
 
 int main(){
 	TGAImage image(width, height, TGAImage::RGB);
-	lecture("/home/maeva/Bureau/Projets/M1 S2/3D/render3d/ressources/african_head/african_head.obj",image);
+	//lecture("/home/maeva/Bureau/Projets/M1 S2/3D/render3d/ressources/african_head/african_head.obj",image);
+	triangle(100,50,300,700,500,750,image,red);
 	image.write_tga_file("img/test.tga");
 	return 0;
 }
