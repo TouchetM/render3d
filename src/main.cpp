78b0c75169d2ab4523a5dd758e243c228f64ad71
Maëva <maeva.touchet@outlook.fr>
2021-02-18 09:45:28 +0100

    Récupération des coordonnées des textures

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ struct Point{
 	int x;
 	int y;
 	int z;
+	int num_texture;
 };
 
 struct Vecteur{
@@ -98,21 +99,23 @@ int min(int a, int b, int c){
 	return b;
 }
 
-void fill_triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, TGAColor couleur){
-	for(int i = min(x0,x1,x2)-50; i <= max(x0,x1,x2)+50 ; i++){
-		for(int j = min(y0,y1,y2)-50; j <= max(y0,y1,y2)+50; j++){
-			if(appartient(x0,y0,x1,y1,x2,y2,i,j)){
+void fill_triangle(Point pt1, Point pt2, Point pt3, TGAImage &image, TGAColor couleur){
+	for(int i = min(pt1.x,pt2.x,pt3.x)-50; i <= max(pt1.x,pt2.x,pt3.x)+50 ; i++){
+		for(int j = min(pt1.y,pt2.y,pt3.y)-50; j <= max(pt1.y,pt2.y,pt3.y)+50; j++){
+			if(appartient(pt1.x,pt1.y,pt2.x,pt2.y,pt3.x,pt3.y,i,j)){
 				image.set(i,j,couleur);
 			}
 		}
 	}
 }
 
-void draw_triangle(int x0, int y0, int z0, int x1, int y1, int z1, int x2, int y2, int z2, TGAImage &image){
+void draw_triangle(Point pt1, Point pt2, Point pt3, vector<Point> coord_textures, TGAImage &image){
+	
+	//Lumière
 	Vecteur v1,v2;
-	v1.x = x1 - x0;	v2.x = x2 - x0;
-	v1.y = y1 - y0;	v2.y = y2 - y0;
-	v1.z = z1 - z0;	v2.z = z2 - z0;
+	v1.x = pt2.x - pt1.x;	v2.x = pt3.x - pt1.x;
+	v1.y = pt2.y - pt1.y;	v2.y = pt3.y - pt1.y;
+	v1.z = pt2.z - pt1.z;	v2.z = pt3.z - pt1.z;
 	
 	Vecteur n = cross(v1,v2);
 	float longueur = sqrt((n.x*n.x)+(n.y*n.y)+(n.z*n.z));
@@ -126,24 +129,33 @@ void draw_triangle(int x0, int y0, int z0, int x1, int y1, int z1, int x2, int y
 	
 	float intensity = n.x*lum.x + n.y*lum.y + n.z*lum.z;	
 	
-	if(intensity > 0)	fill_triangle(x0,y0,x1,y1,x2,y2,image,TGAColor(intensity*255,intensity*255,intensity*255,255));
+	//Textures
+	//TGAImage textures(width, height, TGAImage::RGB);
+	//textures.read_tga_file("/home/maeva/Bureau/Projets/M1 S2/3D/render3d/ressources/african_head/african_head_diffuse.tga");
+	//textures.get(coord_textures[pt1.num_texture].x, coord_textures[pt1.num_texture].y);
+	
+	//Dessin triangle
+	if(intensity > 0)	fill_triangle(pt1,pt2,pt3,image,TGAColor(intensity*255,intensity*255,intensity*255,255));
 
 }
 
 
-void lecture(string name,TGAImage &image){
+void lecture(string name, TGAImage &image){
         string line, mot;
-        int x,y,z,pt1,pt2,pt3;
+        int x,y,z,pt1,pt2,pt3,num;
+        double x2,y2,z2;
         
         //Lecture fichier
         vector<Point> points(0);
+        vector<Point> coord_textures(0);
         ifstream flux2(name);
-        Point point;
+        Point point, texture;
         
         if(flux2){
            while(flux2){
          	flux2 >> mot;
-           	if(mot == "v"){
+           	if(mot == "v"){	//Points
+           	
            		//On récupère le x
            		flux2 >> mot;
            		x = (stof(mot)+1)*300+100;
@@ -163,7 +175,27 @@ void lecture(string name,TGAImage &image){
            		points.push_back(point);
            		
            	}
-           	if(mot == "f"){
+           	if(mot == "vt"){	//Textures
+           	
+           		//On récupère le x
+           		flux2 >> mot;
+           		x2 = stof(mot);
+           	
+           		//On récupère le y
+           		flux2 >> mot;
+           		y2 = stof(mot);
+           		
+           		//On récupère le z
+           		flux2 >> mot;
+           		z2 = stof(mot);
+           		
+           		//On enregistre les coordonnées de la texture
+           		texture.x = x2;
+           		texture.y = y2;
+           		texture.z = z2;
+           		coord_textures.push_back(texture);
+           	}
+           	if(mot == "f"){	//Triangles
            		
            		//Premier point
            		flux2 >> mot;
@@ -171,11 +203,21 @@ void lecture(string name,TGAImage &image){
            		getline(iss1,mot,'/');
 			pt1 = stoi(mot) -1;
 			
+			//Texture point 1
+			getline(iss1,mot,'/');
+			num = stoi(mot) - 1;
+			points[pt1].num_texture = num;
+			
 			//Deuxième point
 			flux2 >> mot;
 			istringstream iss2(mot);
            		getline(iss2,mot,'/');
 			pt2 = stoi(mot) -1;
+			
+			//Texture point 2
+			getline(iss2,mot,'/');
+			num = stoi(mot) - 1;
+			points[pt2].num_texture = num;
 			
 			//Troisième point
 			flux2 >> mot;
@@ -183,8 +225,13 @@ void lecture(string name,TGAImage &image){
            		getline(iss3,mot,'/');
 			pt3 = stoi(mot) -1;
 			
+			//Texture point 3
+			getline(iss3,mot,'/');
+			num = stoi(mot) - 1;
+			points[pt3].num_texture = num;
+			
 			//On dessine le triangle
-			draw_triangle(points[pt1].x, points[pt1].y, points[pt1].z, points[pt2].x,points[pt2].y, points[pt2].z, points[pt3].x, points[pt3].y, points[pt3].z, image);
+			draw_triangle(points[pt1],points[pt2],points[pt3], coord_textures, image);
            	}
            }
         }
